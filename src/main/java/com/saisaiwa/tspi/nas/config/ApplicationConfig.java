272a58e63ab7d6e4878fb24418fba314e0770ab6
chenzedeng <chenzedeng@juzhen.cn>
2024-03-08 17:47:16 +0800

    develop..

--- src/main/java/com/saisaiwa/tspi/nas/config/ApplicationConfig.java
+++ src/main/java/com/saisaiwa/tspi/nas/config/ApplicationConfig.java
@@ -1,10 +1,14 @@
 package com.saisaiwa.tspi.nas.config;
 
 import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,5 +31,17 @@ public class ApplicationConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/ok")
                 .excludePathPatterns("/sys/login")
                 .excludePathPatterns("/ntf");
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }
--- src/test/java/com/saisaiwa/tspi/nas/TspiNasServerApplicationTests.java
+++ src/test/java/com/saisaiwa/tspi/nas/TspiNasServerApplicationTests.java
@@ -1,19 +1,26 @@
 package com.saisaiwa.tspi.nas;
 
 import com.saisaiwa.tspi.nas.domain.entity.User;
-import com.saisaiwa.tspi.nas.service.impl.SessionServiceImpl;
+import com.saisaiwa.tspi.nas.service.impl.UserServiceImpl;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest()
+@ActiveProfiles("dev")
 class TspiNasServerApplicationTests {
 
 
+    @Resource
+    private UserServiceImpl userServiceImpl;
 
     @Test
     void contextLoads() {
-
+        User user = new User();
+        user.setUserAccount("admin");
+        userServiceImpl.generatorPwd(user, "21232f297a57a5a743894a0e4a801fc3");
+        System.out.printf("Salt:%s, Pwd:%s\n",user.getSalt(),user.getUserPassword());
     }
 
 }
